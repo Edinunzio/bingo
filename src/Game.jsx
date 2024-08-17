2fa2a7677f8046eb359e0f19d93aee3bc5a80539
Edinunzio <e.dinunzio@gmail.com>
2024-08-17 18:24:46 -0400

    game, board, cell.

---
+++ src/Game.jsx
@@ -0,0 +1,66 @@
+//import { useState } from 'react'
+
+function Square() {
+
+    return <button className="square">filler text</button>
+}
+
+function Board() {
+
+    return (
+        <>
+            <table className="board">
+                <tbody>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                </tbody>
+            </table>
+        </>
+    )
+}
+
+
+function Game() {
+
+  return (
+    <>
+      <h1>My Custom Bingo Board</h1>
+      <Board />
+    </>
+  )
+}
+
+export default Game
